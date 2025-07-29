6681bc09e371981845e140166526161cd6c609eb
Bhavesh Mali <bhaveshmali2304@gmail.com>
2025-07-30 00:18:05 +0530

    watchHistory controller

--- src/routes/user.router.js
+++ src/routes/user.router.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { userRegister,userLogin,userLogout,tokenRefresher,changePassword,getCurrentUser,updateAccountDetails,updateAvatar,updateCoverImage } from "../controllers/user.controller.js";
+import { userRegister,userLogin,userLogout,tokenRefresher,changePassword,getCurrentUser,updateAccountDetails,updateAvatar,updateCoverImage, getChannelInfo, getWatchHistory } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { jwtVerify } from "../middlewares/auth.middleware.js";
 
@@ -24,9 +24,10 @@ router.route("/login").post(userLogin)
 router.route("/logout").post(jwtVerify,userLogout)
 router.route("/token-refresh").post(tokenRefresher)
 router.route("/change-password").post(jwtVerify,changePassword)
-router.route("/current-user").post(jwtVerify,getCurrentUser)
-router.route("/update-details").post(jwtVerify,updateAccountDetails)
-router.route("/update-avatar").post(jwtVerify,upload.single("avatar"),updateAvatar)
-router.route("/update-coverimage").post(jwtVerify,upload.single("coverImage"),updateCoverImage)
-
+router.route("/current-user").get(jwtVerify,getCurrentUser)
+router.route("/update-details").patch(jwtVerify,updateAccountDetails)
+router.route("/update-avatar").patch(jwtVerify,upload.single("avatar"),updateAvatar)
+router.route("/update-coverimage").patch(jwtVerify, upload.single("coverImage"), updateCoverImage)
+router.route("/channel/:username").get(jwtVerify,getChannelInfo)
+router.route("/history").get(jwtVerify,getWatchHistory)
 export {router}
